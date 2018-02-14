9a74e34f615b95b640a35ba6b4167a40d6e49cd1
Jose Maria <melopsitaco@gmail.com>
2018-02-14 01:26:08 +0100

    Vagrant actualizado

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y r-base jupyter-notebook curl libssl-dev libcurl4-openssl-dev net-tools
-ln -s /usr/bin/jupyter-notebook /usr/bin/jupyter
+apt-get install -y r-base-core curl libssl-dev libcurl4-openssl-dev net-tools python2.7 python-pip python-dev
+pip install --upgrade pip
+pip install jupyter
+
 R -e "install.packages(c('repr','IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'), repos='https://cran.rstudio.com/')"
 R -e "install.packages(c('ggplot','reshape2','entropy','quantmod'), repos='https://cran.rstudio.com/')"
 R -e "devtools::install_github('IRkernel/IRkernel')"
@@ -18,7 +20,7 @@ cat >/usr/lib/systemd/system/jupyter.service <<EOL
      PIDFile=/run/jupyter.pid
      # Step 1 and Step 2 details are here..
      # # ------------------------------------
-     ExecStart=/usr/bin/jupyter-notebook --config=/vagrant --ip=0.0.0.0
+     ExecStart=/usr/local/bin/jupyter notebook --notebook-dir=/vagrant --ip="*" --NotebookApp.token=
      User=vagrant
      Group=vagrant
      WorkingDirectory=/vagrant
